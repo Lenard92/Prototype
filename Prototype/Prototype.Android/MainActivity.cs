9901929493b3ab8a28e483693511d71fb44ce77e
Lenard92 <lenardvwinssen@hotmail.com>
2021-01-20 04:34:19 +0100

    icon en kleine aanpassingen

--- Prototype/Prototype.Android/MainActivity.cs
+++ Prototype/Prototype.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace Prototype.Droid
 {
-    [Activity(Label = "Prototype", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "AgriApp", Icon = "@drawable/image", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
